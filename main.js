1247df5f792eb28c005d63586dc359f7ca456fc3
slopstar <talmudicnetwork@proton.me>
2026-01-12 11:02:48 -0600

    refactored

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-const { downloadImage } = require('./utils/downloadImages.js');
+const { downloadFromUrl } = require('./utils/downloadImages.js');
 const { getMaxPost } = require('./utils/maxPostChecker.js');
-const puppeteer = require('puppeteer');
+const { launchBrowser } = require('./utils/browser.js');
 
-async function main() {
-    // Launch browser once for all downloads
-    const browser = await puppeteer.launch({
-        headless: "new",
-        defaultViewport: null,
-    });
-
-    // Loops through each post
-    // TODO: find max post number prior to operation
-    // TODO: establish how many posts have already been downloaded in prior downloading sessions
+async function main(options = {}) {
+    const { start = 1, end: optEnd } = options;
+    const browser = await launchBrowser();
     try {
-        const urlPrefix = "https://soybooru.com/post/view/"
-        for (let i = 1; i <= 2; i++) {
-            let postUrl = urlPrefix + i;
-            await downloadImage(postUrl, browser);
+        const maxPost = await getMaxPost(browser);
+        const end = typeof optEnd === 'number' && optEnd > 0 ? optEnd : maxPost || start;
+        console.log(`Downloading posts from ${start} to ${end}...`);
+
+        const urlPrefix = 'https://soybooru.com/post/view/';
+        for (let i = start; i <= end; i++) {
+            const postUrl = `${urlPrefix}${i}`;
+            await downloadFromUrl(postUrl, browser, options);
         }
     } finally {
-        // Close browser once at the end
         await browser.close();
-        console.log("Browser closed");
+        console.log('Browser closed');
     }
 }
 
-main().catch(console.error);
+if (require.main === module) {
+    const argv = process.argv.slice(2);
+    const start = argv[0] ? parseInt(argv[0], 10) : 1;
+    const end = argv[1] ? parseInt(argv[1], 10) : undefined;
+    main({ start, end }).catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
+}

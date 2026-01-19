57bef3a3f2d33bdc1561102b0dd624e523f12017
slopstar <talmudicnetwork@proton.me>
2026-01-19 17:28:50 -0600

    moved extractImageTags to downloadImages.js

--- utils/downloadImages.js
+++ utils/downloadImages.js
@@ -83,6 +83,22 @@ async function downloadImages(imageUrls, page, dir) {
 	}
 }
 
+async function extractImageTags(page) {
+    try {
+        const variantName = await page.$eval('#Variantleft > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(2) > a.tag_name', (a) =>
+            a.innerHTML);
+        
+        const tagList = await page.$$eval('#Tagsleft > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) .tag_name', (elements) =>
+            elements.map(el => el.innerHTML));
+
+        return { variant: variantName, tags: tagList };
+    } catch (err) {
+        // Element not found or selector doesn't match
+        console.warn(`Error extracting tags from ${page}: ${err.message}`);
+        return null;
+    }
+}
+
 async function downloadFromUrl(url, browser, options = {}) {
 	const dir = options.dir;
 	if (!dir) {
@@ -104,4 +120,4 @@ async function downloadFromUrl(url, browser, options = {}) {
 	}
 }
 
-module.exports = { ensureDownloadDir, extractImageUrls, downloadImages, downloadFromUrl };
+module.exports = { ensureDownloadDir, extractImageUrls, downloadImages, downloadFromUrl, extractImageTags };

88af9559da5ca7b7ffc0a16f6aee6d09d1c151c8
slopstar <talmudicnetwork@proton.me>
2026-01-26 18:19:34 -0600

    refactored launchBrowser function to include additional launch arguments and removed withBrowser function export

--- utils/browser.js
+++ utils/browser.js
@@ -1,17 +1,12 @@
 const puppeteer = require('puppeteer');
 
 async function launchBrowser(opts = {}) {
-  const defaults = { headless: 'new', defaultViewport: null };
-  return await puppeteer.launch(Object.assign({}, defaults, opts));
+  return puppeteer.launch({
+    headless: 'new',
+    defaultViewport: null,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    ...opts,
+  });
 }
 
-async function withBrowser(fn, opts = {}) {
-  const browser = await launchBrowser(opts);
-  try {
-    return await fn(browser);
-  } finally {
-    await browser.close();
-  }
-}
-
-module.exports = { launchBrowser, withBrowser };
+module.exports = { launchBrowser };

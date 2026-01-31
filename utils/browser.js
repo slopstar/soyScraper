b0caa7026c6b5383335a78fd84e9f8c2e7779ea7
slopstar <talmudicnetwork@proton.me>
2026-01-31 17:29:32 -0600

    refactor headless browser for puppeteer

--- utils/browser.js
+++ utils/browser.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function launchBrowser(opts = {}) {
+  const headless = opts.headless == null ? 'new' : opts.headless;
   return puppeteer.launch({
-    headless: 'new',
+    headless,
     defaultViewport: null,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     ...opts,
